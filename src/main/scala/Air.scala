fb85a4fb20ecb7c1ece9f9dd5888fd3e060a17ff
kaiqiboy <543100804@qq.com>
2022-10-20 16:34:13 +0800

    exp

--- src/main/scala/Air.scala
+++ src/main/scala/Air.scala
@@ -4,8 +4,10 @@ import org.apache.sedona.core.spatialOperator.RangeQuery
 import org.apache.sedona.sql.utils.{Adapter, SedonaSQLRegistrator}
 import org.apache.spark.serializer.KryoSerializer
 import org.locationtech.jts.geom.{Coordinate, Envelope, GeometryFactory}
+
 import java.lang.System.nanoTime
 import org.apache.spark.sql.SparkSession
+
 import scala.io.Source
 
 object Air {
@@ -46,13 +48,20 @@ object Air {
         "PM10_Concentration, NO2_Concentration,CO_Concentration,O3_Concentration,SO2_Concentration, t from input"
       val pointDf = spark.sql(query).repartition(numPartitions)
       val pointRDD = Adapter.toSpatialRdd(pointDf, "location")
+      pointDf.unpersist()
       pointRDD.analyze()
       pointRDD.buildIndex(IndexType.RTREE, false)
       val sQuery = new Envelope(range(0), range(2), range(1), range(3))
       val ts = Range(range(4).toInt, range(5).toInt, 86400).sliding(2).toArray
       val selectedRDD = RangeQuery.SpatialRangeQuery(pointRDD, sQuery, true, true)
-        .rdd.map(x => (x, x.getUserData.asInstanceOf[String], x.getUserData.asInstanceOf[String].split("\t").last.toLong))
-        .filter(x => x._3 >= range(4) && x._3 <= range(5))
+        .rdd.map(x => (x, x.getUserData.asInstanceOf[String].split("\t")(0).toDouble,
+        x.getUserData.asInstanceOf[String].split("\t")(1).toDouble,
+        x.getUserData.asInstanceOf[String].split("\t")(2).toDouble,
+        x.getUserData.asInstanceOf[String].split("\t")(3).toDouble,
+        x.getUserData.asInstanceOf[String].split("\t")(4).toDouble,
+        x.getUserData.asInstanceOf[String].split("\t")(5).toDouble,
+        x.getUserData.asInstanceOf[String].split("\t").last.toLong))
+        .filter(x => x._8 >= range(4) && x._8 <= range(5))
       val map = spark.read.option("delimiter", " ").csv(mapDir).rdd
       val mapRDD = map.map(x => {
         val lsString = x.getString(1)
@@ -61,14 +70,26 @@ object Air {
           new Coordinate(p(0), p(1))
         }
         gf.createLineString(points)
-      })
-      val maps = for ( i <-mapRDD.collect;j<-ts) yield (i,j.toArray)
-      def add(a: Array[Int], b: Array[Int]):Array[Int] = a.zip(b).map { case (x, y) => x + y }
-      val res = selectedRDD.map(x => maps.map{m =>
-        if(m._1.intersects(x._1) && m._2(0)<= x._3 && m._2(1) >= x._3) 1
-        else 0
-      }).reduce(add)
-      println(res.length)
+      }).filter(x => x.intersects(gf.toGeometry(sQuery)))
+      val maps = for (i <- mapRDD.collect; j <- ts) yield (i, j.toArray)
+      map.unpersist()
+      mapRDD.unpersist()
+
+      def add(a: Array[Double], b: Array[Double]): Array[Double] = a.zip(b).map { case (x, y) => x + y }
+
+      def add2(a: Array[Array[Double]], b: Array[Array[Double]]): Array[Array[Double]] = a.zip(b).map { case (x, y) => add(x, y) }
+
+      pointRDD.rawSpatialRDD.unpersist()
+      if (maps.length > 0) {
+        val res = selectedRDD.map(x => maps.map { m =>
+          if (m._1.intersects(x._1) && m._2(0) <= x._8 && m._2(1) >= x._8) Array(x._2, x._3, x._4, x._5, x._6, x._7)
+          else Array(0.0, 0, 0, 0, 0, 0)
+        })
+        val a = Array.ofDim[Double](res.take(1).head.length, 6)
+        val r = res.aggregate(a)(add2, add2)
+        println(r.length)
+      }
+      else println(0)
     }
     println(s"Grid hourly aggregation ${(nanoTime - t) * 1e-9} s")
     sc.stop()
